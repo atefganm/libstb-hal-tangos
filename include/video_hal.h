a03fc0a2cae4d60090e2d0db5a8c59f96a2efd10
atefganm <atef_anim_1974@yahoo.com>
2024-06-10 16:57:20 +0300

    Updated

--- include/video_hal.h
+++ include/video_hal.h
@@ -2,10 +2,10 @@
 #if HAVE_ARM_HARDWARE
 #include "../libarmbox/video_lib.h"
 #include "../libarmbox/hdmi_cec.h"
-#if HAVE_MIPS_HARDWARE
+#elif HAVE_MIPS_HARDWARE
 #include "../libmipsbox/video_lib.h"
 #include "../libmipsbox/hdmi_cec.h"
-#if HAVE_GENERIC_HARDWARE
+#elif HAVE_GENERIC_HARDWARE
 #if BOXMODEL_RASPI
 #include "../libraspi/video_lib.h"
 #else
